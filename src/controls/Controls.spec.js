24a76f07e0976086fcb1cf908d0e74fd54f2dfe6
Daniel Martin <danielmartin1205@gmail.com>
2019-10-30 20:38:07 -0400

    added test suites and snapshot, all test passing

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,21 @@
+import Controls from "./Controls"
+import * as rtl from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import React from 'react';
 // Test away!
+
+test("Close Gate button",  async () => {
+    const wrapper = rtl.render(<Controls />);
+    const close = wrapper.getByText(/close gate/i);
+
+    expect(close).toBeVisible();
+})
+
+test("Lock Gate Button", async () => {
+    const wrapper = rtl.render(<Controls />);
+    const lockButton = wrapper.getByText(/lock gate/i)
+
+    expect(lockButton.disabled).toBeTruthy();
+
+    expect(lockButton).toBeVisible();
+})
